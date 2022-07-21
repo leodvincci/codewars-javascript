0c34c1c82757509d71b1d9f58cf9427667d09e07
Leo <lpenrose@depaul.edu>
2022-07-21 01:07:28 -0500

    completed new code war

---
+++ 8kyu/A-Needle-in-the-Haystack.js
@@ -0,0 +1,16 @@
+function toCamelCase(str){
+    let changed = "";
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === "-" || str[i] === "-"){
+
+        }else{
+            console.log(str[i])
+
+            changed[i] = str[i];
+        }
+    }
+    console.log(changed)
+    return changed;
+}
+
+toCamelCase("the-stealth-warrior");
